49726d04542f0a8d969323445e8070f728de5a32
Damian McDonald <damian.mcdonald1979@gmail.com>
2019-05-07 12:28:24 +0200

    Initial commit

---
+++ metricbeat/conf/setup-beat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+beat=metricbeat
+
+until curl -s "http://kibana:5601/login" | grep "Loading Kibana" > /dev/null; do
+	  echo "Waiting for kibana..."
+	  sleep 1
+done
+
+echo "Setting up dashboards..."
+# Load the sample dashboards for the Beat.
+# REF: https://www.elastic.co/guide/en/beats/metricbeat/master/metricbeat-sample-dashboards.html
+${beat} --strict.perms=false setup -v
